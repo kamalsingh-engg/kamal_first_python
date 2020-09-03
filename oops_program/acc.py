8bbde9b4cc437bae719be7fe24f23b3b3c6c97cb
u18m90 <444kamalsingh@gmail,com>
2020-09-03 17:11:52 +0530

    oops_program

---
+++ oops_program/acc.py
@@ -0,0 +1,25 @@
+class account:
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        with open(filepath,'r') as file:
+            self.balance = int(file.read())
+
+
+
+    def deposit(self,amount):
+        self.balance =self.balance + amount
+
+    def withdraw(self,amount):
+        self.balance = self.balance - amount
+
+    def commit(self):
+        with open(self.filepath,'w') as file:
+            file.write(str(self.balance))
+
+
+account = account("D://pycharm//gettingstarted//oops_program//balance.txt");
+print(account.balance)
+account.deposite(50)
+print(account.balance)
+account.commit()
